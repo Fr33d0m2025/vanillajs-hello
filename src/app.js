04857ab3885bebdd4ba39a8a5121826c902acac4
Johan Santallana <johan921229@gmail.com>
2025-06-21 17:05:15 -0400

    Resolucion del ejercicio de generar excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import "bootstrap";
-import "./style.css";
+window.onload = function () {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
+  const quien = who[Math.floor(Math.random() * who.length)];
+  const accion = action[Math.floor(Math.random() * action.length)];
+  const que = what[Math.floor(Math.random() * what.length)];
+  const cuando = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const excuse = quien + " " + accion + " " + que + " " + cuando;
+  document.getElementById("excuse").innerHTML = excuse;
+}; 
